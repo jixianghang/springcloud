e83f48e0658d2a951c2530456cacc80a9407d4bf
jixianghang <15618729695@163.com>
2020-09-08 11:14:29 +0800

    init

--- common/src/main/java/com/jixh/common/util/SensitiveWordInit.java
+++ common/src/main/java/com/jixh/common/util/SensitiveWordInit.java
@@ -11,137 +11,139 @@ import java.util.Map;
 import java.util.Set;
 
 /**
- * @Description: ???????????????HashMap????DFA????
- * @Project?test
+ * @Description: 初始化敏感词库，将敏感词加入到HashMap中，构建DFA算法模型
+ * @Project：test
  * @Author : chenming
- * @Date ? 2014?4?20? ??2:27:06
+ * @Date ： 2014年4月20日 下午2:27:06
  * @version 1.0
  */
 public class SensitiveWordInit {
-	private String ENCODING = "GBK";    //????
-	@SuppressWarnings("rawtypes")
-	public HashMap sensitiveWordMap;
+    private String ENCODING = "UTF-8";    //字符编码
+    @SuppressWarnings("rawtypes")
+    public HashMap sensitiveWordMap;
 
-	public SensitiveWordInit(){
-		super();
-	}
+    public SensitiveWordInit(){
+        super();
+    }
 
-	/**
-	 * @author chenming
-	 * @date 2014?4?20? ??2:28:32
-	 * @version 1.0
-	 */
-	@SuppressWarnings("rawtypes")
-	public Map initKeyWord(){
-		try {
-			//??????
-			Set<String> keyWordSet = readSensitiveWordFile();
-			//????????HashMap?
-			addSensitiveWordToHashMap(keyWordSet);
-			//spring??application???application.setAttribute("sensitiveWordMap",sensitiveWordMap);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return sensitiveWordMap;
-	}
+    /**
+     * @author chenming
+     * @date 2014年4月20日 下午2:28:32
+     * @version 1.0
+     */
+    @SuppressWarnings("rawtypes")
+    public Map initKeyWord(){
+        try {
+            //读取敏感词库
+            Set<String> keyWordSet = readSensitiveWordFile();
+            //将敏感词库加入到HashMap中
+            addSensitiveWordToHashMap(keyWordSet);
+            //spring获取application，然后application.setAttribute("sensitiveWordMap",sensitiveWordMap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return sensitiveWordMap;
+    }
 
-	/**
-	 * ?????????????HashSet??????DFA?????<br>
-	 * ? = {
-	 *      isEnd = 0
-	 *      ? = {<br>
-	 *        isEnd = 1
-	 *           ? = {isEnd = 0
-	 *                ? = {isEnd = 1}
-	 *                }
-	 *           ?  = {
-	 *               isEnd = 0
-	 *             ? = {
-	 *               isEnd = 1
-	 *              }
-	 *            }
-	 *           }
-	 *      }
-	 *  ? = {
-	 *      isEnd = 0
-	 *      ? = {
-	 *       isEnd = 0
-	 *       ? = {
-	 *              isEnd = 0
-	 *              ? = {
-	 *                   isEnd = 1
-	 *                  }
-	 *              }
-	 *       }
-	 *      }
-	 * @author chenming
-	 * @date 2014?4?20? ??3:04:20
-	 * @param keyWordSet  ????
-	 * @version 1.0
-	 */
-	@SuppressWarnings({ "rawtypes", "unchecked" })
-	private void addSensitiveWordToHashMap(Set<String> keyWordSet) {
-		sensitiveWordMap = new HashMap(keyWordSet.size());     //???????????????
-		String key = null;
-		Map nowMap = null;
-		Map<String, String> newWorMap = null;
-		//??keyWordSet
-		Iterator<String> iterator = keyWordSet.iterator();
-		while(iterator.hasNext()){
-			key = iterator.next();    //???
-			nowMap = sensitiveWordMap;
-			for(int i = 0 ; i < key.length() ; i++){
-				char keyChar = key.charAt(i);       //???char?
-				Object wordMap = nowMap.get(keyChar);       //??
+    /**
+     * 读取敏感词库，将敏感词放入HashSet中，构建一个DFA算法模型：<br>
+     * 中 = {
+     *      isEnd = 0
+     *      国 = {<br>
+     *        isEnd = 1
+     *           人 = {isEnd = 0
+     *                民 = {isEnd = 1}
+     *                }
+     *           男  = {
+     *               isEnd = 0
+     *             人 = {
+     *               isEnd = 1
+     *              }
+     *            }
+     *           }
+     *      }
+     *  五 = {
+     *      isEnd = 0
+     *      星 = {
+     *       isEnd = 0
+     *       红 = {
+     *              isEnd = 0
+     *              旗 = {
+     *                   isEnd = 1
+     *                  }
+     *              }
+     *       }
+     *      }
+     * @author chenming
+     * @date 2014年4月20日 下午3:04:20
+     * @param keyWordSet  敏感词库
+     * @version 1.0
+     */
+    @SuppressWarnings({ "rawtypes", "unchecked" })
+    private void addSensitiveWordToHashMap(Set<String> keyWordSet) {
+        sensitiveWordMap = new HashMap(keyWordSet.size());     //初始化敏感词容器，减少扩容操作
+        String key = null;
+        Map nowMap = null;
+        Map<String, String> newWorMap = null;
+        //迭代keyWordSet
+        Iterator<String> iterator = keyWordSet.iterator();
+        while(iterator.hasNext()){
+            key = iterator.next();    //关键字
+            nowMap = sensitiveWordMap;
+            for(int i = 0 ; i < key.length() ; i++){
+                char keyChar = key.charAt(i);       //转换成char型
+                Object wordMap = nowMap.get(keyChar);       //获取
 
-				if(wordMap != null){        //?????key?????
-					nowMap = (Map) wordMap;
-				}
-				else{     //??????????map????isEnd???0??????????
-					newWorMap = new HashMap<String,String>();
-					newWorMap.put("isEnd", "0");     //??????
-					nowMap.put(keyChar, newWorMap);
-					nowMap = newWorMap;
-				}
+                if(wordMap != null){        //如果存在该key，直接赋值
+                    nowMap = (Map) wordMap;
+                }
+                else{     //不存在则，则构建一个map，同时将isEnd设置为0，因为他不是最后一个
+                    newWorMap = new HashMap<String,String>();
+                    newWorMap.put("isEnd", "0");     //不是最后一个
+                    nowMap.put(keyChar, newWorMap);
+                    nowMap = newWorMap;
+                }
 
-				if(i == key.length() - 1){
-					nowMap.put("isEnd", "1");    //????
-				}
-			}
-		}
-	}
+                if(i == key.length() - 1){
+                    nowMap.put("isEnd", "1");    //最后一个
+                }
+            }
+        }
+    }
 
-	/**
-	 * ?????????????????set???
-	 * @author chenming
-	 * @date 2014?4?20? ??2:31:18
-	 * @return
-	 * @version 1.0
-	 * @throws Exception
-	 */
-	@SuppressWarnings("resource")
-	private Set<String> readSensitiveWordFile() throws Exception{
-		Set<String> set = null;
+    /**
+     * 读取敏感词库中的内容，将内容添加到set集合中
+     * @author chenming
+     * @date 2014年4月20日 下午2:31:18
+     * @return
+     * @version 1.0
+     * @throws Exception
+     */
+    @SuppressWarnings("resource")
+    private Set<String> readSensitiveWordFile() throws Exception{
+        Set<String> set = null;
 
-		File file = new File("D:\\SensitiveWord.txt");    //????
-		InputStreamReader read = new InputStreamReader(new FileInputStream(file),ENCODING);
-		try {
-			if(file.isFile() && file.exists()){      //???????
-				set = new HashSet<String>();
-				BufferedReader bufferedReader = new BufferedReader(read);
-				String txt = null;
-				while((txt = bufferedReader.readLine()) != null){    //?????????????set?
-					set.add(txt);
-				}
-			}
-			else{         //?????????
-				throw new Exception("?????????");
-			}
-		} catch (Exception e) {
-			throw e;
-		}finally{
-			read.close();     //?????
-		}
-		return set;
-	}
+
+        String path = SensitiveWordInit.class.getClassLoader().getResource("SensitiveWord.txt").getPath();
+        File file = new File(path);    //读取文件
+        InputStreamReader read = new InputStreamReader(new FileInputStream(file),ENCODING);
+        try {
+            if(file.isFile() && file.exists()){      //文件流是否存在
+                set = new HashSet<String>();
+                BufferedReader bufferedReader = new BufferedReader(read);
+                String txt = null;
+                while((txt = bufferedReader.readLine()) != null){    //读取文件，将文件内容放入到set中
+                    set.add(txt);
+                }
+            }
+            else{         //不存在抛出异常信息
+                throw new Exception("敏感词库文件不存在");
+            }
+        } catch (Exception e) {
+            throw e;
+        }finally{
+            read.close();     //关闭文件流
+        }
+        return set;
+    }
 }
--- common/src/main/java/com/jixh/common/util/SensitivewordFilter.java
+++ common/src/main/java/com/jixh/common/util/SensitivewordFilter.java
@@ -1,5 +1,7 @@
 package com.jixh.common.util;
 
+import com.alibaba.fastjson.JSON;
+
 import java.util.HashSet;
 import java.util.Iterator;
 import java.util.Map;
@@ -151,6 +153,8 @@ public class SensitivewordFilter {
     public static void main(String[] args) {
         SensitivewordFilter filter = new SensitivewordFilter();
         System.out.println("敏感词的数量：" + filter.sensitiveWordMap.size());
+        System.out.println("敏感词：" + JSON.toJSONString(filter.sensitiveWordMap));
+
         String string = "太多的伤感情怀也许只局限于饲养基地 荧幕中的情节，主人公尝试着去用某种方式渐渐的很潇洒地释自杀指南怀那些自己经历的伤感。"
                 + "然后法轮功 我们的扮演的角色就是跟随着主人公的喜红客联盟 怒哀乐而过于牵强的把自己的情感也附加于银幕情节中，然后感动就流泪，"
                 + "难过就躺在某一个人的怀里尽情的阐述心扉或者手机卡复制器一个人一杯红酒一部电影在夜三级片廖伯年 北京刘广智 空军 深人静的晚上，关上电话静静的发呆着。";
